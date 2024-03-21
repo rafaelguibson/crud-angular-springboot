bfb9328cea30913807e28a967c394527b649128d
Rafael Guibson <rafaelguibsons@gmail.com>
2024-03-21 15:51:54 -0300

    Projeto Crud Basico com Springboot e Angular

---
+++ crud-springboot/src/main/java/com/crud/crudspringboot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.crud.crudspringboot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CrudSpringbootApplication.class);
+	}
+
+}
